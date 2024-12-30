66805176a2fdeec2e6077a410ab49020dbacd0c3
Kunal7069 <107999879+Kunal7069@users.noreply.github.com>
2024-12-30 16:57:58 +0530

    Update app.py

--- app.py
+++ app.py
@@ -49,71 +49,6 @@ def get_colors():
         }), 500
     
 
-# @app.route('/getOutfit', methods=['POST'])
-# def predict():
-#     try:
-#         # Get JSON data from request
-#         manual_input = request.json
-#         Outfit=manual_input.get('Outfit')
-#         Style=manual_input.get('Style')
-#         Watch=manual_input.get('Watch')
-#         Belt=manual_input.get('Belt')
-#         Shoes=manual_input.get('Shoes')
-#         preferred_shirt = manual_input.get('preferred_shirt', [])
-#         preferred_pants = manual_input.get('preferred_pants', [])
-#         preferred_shoes = manual_input.get('preferred_shoes', [])
-        
-#         if Outfit=='FORMAL':
-#             database=Outfit+'_'+Style
-#         else:
-#             database=Outfit
-#         collection=db[database]
-#         query = {
-#             "outfit_input": {"$regex": f"^{Outfit}$", "$options": "i"},
-#             "style_input": {"$regex": f"^{Style}$", "$options": "i"},
-#             "watch_input": {"$regex": f"^{Watch}$", "$options": "i"},
-#             "belt_input": {"$regex": f"^{Belt}$", "$options": "i"},
-#             "shoes_input": {"$regex": f"^{Shoes}$", "$options": "i"}
-#         }
-
-#         # Add case-insensitive filters for preferred shirt and pants colors
-#         if len(preferred_shirt)>0:
-#             query["shirt_output"] = {"$in": [re.compile(f"^{color}$", re.IGNORECASE) for color in preferred_shirt]}
-#         if len(preferred_pants)>0:
-#             query["pant_output"] = {"$in": [re.compile(f"^{color}$", re.IGNORECASE) for color in preferred_pants]}
-#         if len(preferred_shoes)>0:
-#             query["shoes_output"] = {"$in": [re.compile(f"^{color}$", re.IGNORECASE) for color in preferred_shoes]}
-
-
-#         # Query the database
-#         matched_entries = list(collection.find(query, {"_id": 0})) 
-        
-#         if not matched_entries:
-#             return jsonify({
-#                 "status": "success",
-#                 "message": "No matching entries found.",
-#                 "data": []
-#             })
-        
-#         processed_data = []
-#         for entry in matched_entries:
-#             processed_entry = {}
-#             for key, value in entry.items():
-#                 if key.endswith('_output'):
-#                     new_key = key.replace('_output', '')  
-#                     processed_entry[new_key] = value
-#             processed_data.append(processed_entry)
-        
-#         return jsonify({
-#             "data": processed_data
-#         })
-#     except ValueError as e:
-#         return jsonify({"status": "error", "message": str(e)}), 400
-#     except Exception as e:
-#         return jsonify({"status": "error", "message": "An unexpected error occurred."}), 500
-
-
-
 @app.route('/getOutfit', methods=['POST'])
 def predict():
     try:
@@ -124,12 +59,12 @@ def predict():
         Watch=manual_input.get('Watch')
         Belt=manual_input.get('Belt')
         Shoes=manual_input.get('Shoes')
-        preferred_shirt = manual_input.get('preferred_shirt')
-        preferred_shoes = manual_input.get('preferred_shoes')
+        preferred_shirt = manual_input.get('preferred_shirt',[])
+        preferred_shoes = manual_input.get('preferred_shoes',[])
         if Outfit == 'FORMAL':
-            preferred_pants = manual_input.get('preferred_pants')
+            preferred_pants = manual_input.get('preferred_pants',[])
         elif Outfit == 'CASUAL':
-            preferred_pants = manual_input.get('preferred_jeans')
+            preferred_pants = manual_input.get('preferred_jeans',[])
         
         
         if Outfit=='FORMAL':
@@ -137,6 +72,7 @@ def predict():
         else:
             database=Outfit
         collection=db[database]
+        print("1")
         query = {
             "outfit_input": {"$regex": f"^{Outfit}$", "$options": "i"},
             "style_input": {"$regex": f"^{Style}$", "$options": "i"},
@@ -144,7 +80,7 @@ def predict():
             "belt_input": {"$regex": f"^{Belt}$", "$options": "i"},
             "shoes_input": {"$regex": f"^{Shoes}$", "$options": "i"}
         }
-
+        print("2")
         # Add case-insensitive filters for preferred shirt and pants colors
         if len(preferred_shirt)>0:
             query["shirt_output"] = {"$in": [re.compile(f"^{color}$", re.IGNORECASE) for color in preferred_shirt]}
@@ -156,7 +92,7 @@ def predict():
 
         # Query the database
         matched_entries = list(collection.find(query, {"_id": 0})) 
-        
+        print("3")
         if not matched_entries:
             return jsonify({
                 "status": "success",
@@ -172,19 +108,37 @@ def predict():
                     new_key = key.replace('_output', '')  
                     processed_entry[new_key] = value
             processed_data.append(processed_entry)
-        print(processed_data)
         if Outfit == 'CASUAL':
             for entry in processed_data:
                 if 'pant' in entry:
                     entry['jeans'] = entry.pop('pant')
+        for i in processed_data:
+            i["shoes"] = f"{Outfit}_{i['shoes']}"
+        result_1=[]    
+        if Style=='NO' or Style=='QUIRKY':
+            for item in processed_data:
+                 if item["belt"] == "BLACK":
+                    new_item = item.copy()
+                    new_item["belt"] = "BROWN"
+                    result_1.append(new_item)
+                 elif item["belt"] == "BROWN":
+                    new_item = item.copy()
+                    new_item["belt"] = "BLACK"
+                    result_1.append(new_item)
+        if Style=='PROFESSIONAL' or Style=='MINIMAL':
+            for i in processed_data:
+                if i['belt']=="BROWN":
+                    i['belt']="BLACK"
+            
+        combine_data=processed_data+result_1
         return jsonify({
-            "data": processed_data
+            "data": combine_data
         })
 
     except ValueError as e:
         return jsonify({"status": "error", "message": str(e)}), 400
     except Exception as e:
-        return jsonify({"status": "error", "message": "An unexpected error occurred."}), 500
+        return jsonify({"status": "error", "message": str(e)}), 500
 
 
 
